5b2c66c4eacc84f64cad93566b3dd4c9950a1bb3
Adie-Stella <stellattoe@gmail.com>
2022-12-08 20:41:53 -0800

    Fine task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: always 0
-*/
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+   */
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+
+
 }
